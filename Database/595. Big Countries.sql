738ba56b23d133a36e83859d4be87bab284eefbd
Aditya Prajapati <princeak22486@gmail.com>
2024-05-28 08:53:07 +0530

    595. Big Countries

--- Database/595. Big Countries.sql
+++ Database/595. Big Countries.sql
@@ -1,3 +1,5 @@
+--https://leetcode.com/problems/big-countries/?envType=study-plan-v2&envId=top-sql-50
+
 Select name, population, area
 FROM World
 WHERE area >= "3000000" or population >= "25000000"
