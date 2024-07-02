6fa22e33bf4d182941fa9ef0bbc5743fa128d426
Aditya Prajapati <princeak22486@gmail.com>
2024-07-02 22:13:03 +0530

    275. H-Index II

---
+++ Array/275. H-Index II.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/h-index-ii/description/
+
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.rbegin(), citations.rend());
+        int h = 0;
+        while (h < citations.size() && citations[h] > h)
+        {
+            h++;
+        }
+        return h;
+    }
+};
