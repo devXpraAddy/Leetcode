1fc8d42e69fa25cb6817759332b9d6e86e486de6
Aditya Prajapati <princeak22486@gmail.com>
2024-07-22 22:19:36 +0530

    202. Happy Number

---
+++ Array/202. Happy Number.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/happy-number/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution
+{
+private:
+    int nextNumber(int n)
+    {
+        int newNumber = 0;
+        while (n != 0)
+        {
+            int num = n % 10;
+            newNumber += num * num;
+            n = n / 10;
+        }
+        return newNumber;
+    }
+
+public:
+    bool isHappy(int n)
+    {
+        unordered_set<int> set;
+        while (n != 1 && !set.count(n))
+        {
+            set.insert(n);
+            n = nextNumber(n);
+        }
+        return n == 1;
+    }
+};
