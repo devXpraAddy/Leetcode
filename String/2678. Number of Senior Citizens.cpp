a096a3ebf07d7867b0ab87addda4657bbeaddb26
Aditya Prajapati <princeak22486@gmail.com>
2024-08-01 23:02:04 +0530

    2678. Number of Senior Citizens

---
+++ String/2678. Number of Senior Citizens.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/number-of-senior-citizens/description/?envType=daily-question&envId=2024-08-01
+
+class Solution
+{
+public:
+    int countSeniors(vector<string> &details)
+    {
+        int count = 0;
+        for (int i = 0; i < details.size(); i++)
+        {
+            int age = stoi(details[i].substr(11, 2));
+            if (age > 60)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
