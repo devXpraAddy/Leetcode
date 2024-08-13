ce414e7b41ec4107c3820e44f0366f162e0aeb6b
Aditya Prajapati <princeak22486@gmail.com>
2024-08-13 22:12:29 +0530

    40. Combination Sum II

---
+++ Backtracking/40. Combination Sum II.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/combination-sum-ii/description/?envType=daily-question&envId=2024-08-13
+
+class Solution
+{
+public:
+    void solve(vector<int> &candidates, int target, vector<int> curr, vector<vector<int>> &result, int idx)
+    {
+        if (target < 0)
+            return;
+        if (target == 0)
+        {
+            result.push_back(curr);
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); i++)
+        {
+            if (i > idx && candidates[i] == candidates[i - 1])
+                continue; // ignore duplicate elements
+            curr.push_back(candidates[i]);
+            solve(candidates, target - candidates[i], curr, result, i + 1);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> result;
+        vector<int> curr;
+        sort(candidates.begin(), candidates.end()); // because we will ignore duplicate elements
+        solve(candidates, target, curr, result, 0);
+        return result;
+    }
+};
