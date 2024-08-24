d10fe0f7547652f759ae94d03bf164bd36f55158
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2024-08-24 14:43:55 +0530

    Time: 42 ms (44.35%), Space: 13.2 MB (92.51%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int t[13][10001];
+    int solve(vector<int>& coins, int idx, int amount){
+        if(amount == 0) return 0;
+        if(idx == coins.size()) return INT_MAX-1;
+        int result = 0;
+
+        if(t[idx][amount] != -1){
+            return t[idx][amount];
+        }
+
+        if(coins[idx] > amount){
+            int skip = solve(coins, idx+1, amount);
+            result = skip;
+        }else{
+            int take = 1+ solve(coins, idx, amount - coins[idx]);
+            int skip = solve(coins, idx+1, amount);
+            result = min(take, skip);
+        }
+        return t[idx][amount] = result;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        memset(t, -1, sizeof(t));
+        int result = solve(coins, 0, amount);
+        return result == INT_MAX -1? -1 : result;
+    }
+};
+
