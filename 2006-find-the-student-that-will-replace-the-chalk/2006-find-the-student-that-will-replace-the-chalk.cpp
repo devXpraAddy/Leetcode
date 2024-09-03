907c3469f8c4692a556cddf95b06b1e5f8936afa
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2024-09-03 08:22:25 +0530

    Time: 96 ms (57.45%), Space: 77.1 MB (18.85%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int n = chalk.size();
-
-        long long totalChalkSum = 0; //Total chalks required in one iteration
-        for(int &chalkReq : chalk) { //O(n)
-            totalChalkSum += chalkReq;
+        long long sum  = 0;
+        for(int i = 0; i<chalk.size(); i++){
+            sum+= chalk[i];
         }
-
-        //Number of full iteration possible = k/totalChalkSum
-
-        int remainChalk = k%totalChalkSum;
-
-        for(int i = 0; i < n; i++) { //O(n)
-            if(remainChalk < chalk[i]) {
+        k = (long long)k%sum;
+        for(int i = 0;i<chalk.size(); i++){
+            if((long long)k < chalk[i]){
                 return i;
             }
-
-            remainChalk -= chalk[i];
+            k -= chalk[i];
         }
-
         return -1;
     }
 };
-
