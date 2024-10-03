a80ab0fe9130f94220260a615b67e1790e7aa792
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2024-10-03 12:37:24 +0530

    Time: 5 ms (24.91%), Space: 10.4 MB (33.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> zero;
-        vector<int> one;
-        vector<int> two;
-        vector<int> ans;
-        for(int i = 0; i<nums.size(); i++){
+        int ct0 = 0;
+        int ct1 = 0;
+        int ct2 = 0;
+
+        for(int i = 0 ;i<nums.size(); i++){
             if(nums[i] == 0){
-                zero.push_back(nums[i]);
-            }
-            if(nums[i] == 1){
-                one.push_back(nums[i]);
-            }
-            if(nums[i] == 2){
-                two.push_back(nums[i]);
+                ct0++;
+            }else if(nums[i] == 1){
+                ct1++;
+            }else if(nums[i] == 2){
+                ct2++;
             }
         }
-        for(int i = 0; i<zero.size(); i++){
-            ans.push_back(zero[i]);
+        for(int i = 0 ; i<ct0; i++){
+            nums[i] = 0;
         }
-        for(int i = 0; i<one.size(); i++){
-            ans.push_back(one[i]);
+        for(int i = ct0; i< ct0+ct1; i++){
+            nums[i] = 1;
         }
-        for(int i = 0; i<two.size(); i++){
-            ans.push_back(two[i]);
-        }
-        for(int i = 0; i<ans.size(); i++){
-            nums[i] = ans[i];
+        for(int i = ct0+ct1; i<nums.size(); i++){
+            nums[i] = 2;
         }
     }
 };
