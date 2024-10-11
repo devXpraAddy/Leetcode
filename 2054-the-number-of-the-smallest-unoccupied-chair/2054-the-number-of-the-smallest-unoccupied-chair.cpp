35afaa07e478d85d4e5acf193500204ae0fbaed7
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2024-10-11 22:53:13 +0530

    Time: 304 ms (13.75%), Space: 54.5 MB (97.59%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,38 @@
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+        
+        vector<int> endTimes(n, -1); //at max we will have 0 to n-1 chairs
+        
+        /*
+            We need to sort the times based on arrival time but we don't want to
+            loose the friend number after sorting. So, store the arrival time
+            of targetFriend because it's given in the question that 
+            Each arrival time is distinct.
+        */
+        
+        int targetArrivalTime = times[targetFriend][0];
+        
+        sort(begin(times), end(times));
+        
+        for(vector<int> &time : times) {
+            int arrival = time[0];
+            int depart  = time[1];
+            //Find the first unoccupied chair
+            for(int i = 0; i<n; i++) {
+                if(endTimes[i] <= arrival) {
+                    endTimes[i] = depart; //update with current friend depart time
+                    
+                    if(arrival == targetArrivalTime)
+                        return i;
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
