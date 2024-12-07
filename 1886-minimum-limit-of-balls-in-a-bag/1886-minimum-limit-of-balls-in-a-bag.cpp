a0d7e9632c9b56816057abbc2b3925702f4515e3
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2024-12-07 23:10:19 +0530

    Time: 35 ms (54.67%), Space: 59.7 MB (28.46%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,45 @@
+//T.C : O(nlog(MAX)), where n = length of nums, MAX = max value in nums
+//S.C : O(1)
+class Solution {
+public:
+
+    bool isPossible(vector<int>& nums, int maxOps, int mid) {
+        int totalOps = 0; //to bring each number <= mid
+
+        for(int &num : nums) {
+            int ops = num / mid;
+
+            if(num % mid == 0) {
+                ops -= 1;
+            }
+
+            totalOps += ops;
+        }
+
+        return totalOps <= maxOps;
+    }
+
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int l = 1;
+        int r = *max_element(begin(nums), end(nums));
+        
+        int result = r; //we have to minimize this
+
+        //O(n * log(MAX))
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(isPossible(nums, maxOperations, mid) == true) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+
+        return result;
+
+
+    }
+};
+
