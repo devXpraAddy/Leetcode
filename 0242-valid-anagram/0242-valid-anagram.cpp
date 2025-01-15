860e9e8b06220c2de0f65340dada4052a3d0cdb1
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-01-15 10:03:10 +0530

    Time: 4 ms (45.43%), Space: 9.8 MB (28.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int>mp;
+        for(int i =0 ; i<s.size(); i++){
+            mp[s[i] - 'a']++;
+        }
+        for(int i = 0; i<t.size(); i++){
+            mp[t[i] - 'a']--;
+        }
+        for(auto it: mp){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
