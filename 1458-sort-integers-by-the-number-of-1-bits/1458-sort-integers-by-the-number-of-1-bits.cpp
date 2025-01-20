720ff0cafedde347604fcc50ec710c9d4e0d3f28
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-01-20 12:59:54 +0530

    Time: 0 ms (100%), Space: 14.9 MB (21.39%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
         vector<pair<int,int>> vec;
-        vector<int>res;
-
-        for(int i =0 ; i<arr.size(); i++){
-            int cnt = 0;
+        vector<int> res;
+        for(int i =0; i<arr.size(); i++){
             int num = arr[i];
-            while(num >0){
-                num = num & (num-1);
+            int cnt =0;
+            while(num>0){
+                num &= (num-1);
                 cnt++;
             }
             vec.push_back({cnt, arr[i]});
         }
         sort(vec.begin(), vec.end());
-
-        for(auto& x: vec){
+        for(auto x: vec){
             res.push_back(x.second);
         }
         return res;
