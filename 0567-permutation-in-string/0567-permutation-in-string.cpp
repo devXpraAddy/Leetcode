16ef83c48f19c5435a6b3ad64ecee9cfe4aaac20
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-01-28 18:16:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (43.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,47 +1,35 @@
-//T.C : O(m+n)
-//S.C : O(26)
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        int n = s1.length();
-        int m = s2.length();
-
-        // If s1 is larger than s2, no permutation can exist
-        if (n > m) return false;
-
-        // Frequency vectors for s1 and the current window in s2
-        vector<int> s1_freq(26, 0);
-        vector<int> s2_freq(26, 0);
-
-        // Fill frequency of characters in s1
-        for (int i = 0; i < n; i++) {
-            s1_freq[s1[i] - 'a']++;
+    bool allzero(vector<int> & cnt){
+        for(int i= 0; i<cnt.size(); i++){
+            if(cnt[i] != 0){
+                return false;
+            }
         }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int n = s2.size();
+        int k = s1.size();
 
-        // Slide the window over s2
-        int i = 0; // left index of the sliding window
-        int j = 0; // right index of the sliding window
-        while (j < m) {
-            // Include a new character from the right end of the window
-            s2_freq[s2[j] - 'a']++;
+        vector<int> cnt(26,0);
 
-            // Check if the current window size matches the size of s1
-            if (j - i + 1 > n) {
-                // If we have passed the size of s1, we need to remove the leftmost character
-                s2_freq[s2[i] - 'a']--;
+        for(int i = 0; i<s1.size(); i++){
+            cnt[s1[i]- 'a']++;
+        }
+        int i = 0;
+        int j = 0;
+        while(j<n){
+            cnt[s2[j] - 'a']--;
+            if(j-i+1 == k){
+                if(allzero(cnt)){
+                    return true;
+                }
+                cnt[s2[i] - 'a']++;
                 i++;
             }
-
-            // Check if the current window's frequency matches s1's frequency
-            if (s1_freq == s2_freq) {
-                return true;
-            }
-
             j++;
         }
-
-        // No matching window found
         return false;
     }
 };
-
