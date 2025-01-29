04510ed929d0834b079e47c8d941f7827953d7dc
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-01-29 11:47:51 +0530

    Time: 8 ms (15.35%), Space: 14.8 MB (13.24%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
-
-    bool dfs(unordered_map<int, vector<int>> &adj, int u, int v, vector<bool>& visited) {
+    bool dfs(unordered_map<int, vector<int>>& adj, int u, int v, vector<bool>& visited){
         visited[u] = true;
-
-        if(u == v) {
-            return true;
+        if(u == v){
+            return true;  // that means connected
         }
-
-        for(int &ngbr : adj[u]) {
-            if(visited[ngbr]) continue;
-
-            if(dfs(adj, ngbr, v, visited)) {
+        for(int & neig: adj[u]){
+            if(visited[neig]) continue; // check for visited
+            if(dfs(adj, neig, v, visited)){ // if not visited visit it
                 return true;
             }
         }
-
         return false;
-
     }
 
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        //number of nodes = n
-        //number of edges = n
-
         unordered_map<int, vector<int>> adj;
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++){
             int u = edges[i][0];
             int v = edges[i][1];
-            
-            vector<bool> visited(n, false);
-            if(adj.find(u) != adj.end() && adj.find(v) != adj.end() && dfs(adj, u, v, visited)) {
+            vector<bool> visited(n,false);
+
+
+            if(adj.find(u) != adj.end() && adj.find(v) != adj.end() && dfs(adj, u, v, visited)){
                 return edges[i];
             }
-
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
         return {};
     }
 };
