163b2e7d5ba88aef41e388af9effb09b8c7fdad5
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-02-05 23:42:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.33%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,26 +1,38 @@
+//T.C : O(n)
+//S.C : O(26) ~= O(1)
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        int n = s1.size();
-        int cnt = 0;
-        vector<int> charr(26,0);
+        int n = s1.length();
 
-        for (int i = 0; i < n; i++) {
-            charr[s1[i]-'a']++;
-            if (s1[i] != s2[i]) {
-                cnt++;
-            }
+        if(s1 == s2) {
+            return true;
         }
 
-        for (char& ch : s2) {
-            if (charr[ch-'a']==0) {
+        int diffs = 0;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+
+        for(int i = 0; i < n; i++) {
+            if(s1[i] != s2[i]) {
+                diffs++;
+            }
+
+            if(diffs > 2)
+                return false;
+
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] != freq2[i]) {
                 return false;
             }
-            charr[ch-'a']--;
         }
 
-        if (cnt == 0 || cnt == 2)
-            return true;
-        return false;
+        return true;
+
     }
 };
