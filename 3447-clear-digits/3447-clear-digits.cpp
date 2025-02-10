5a82cf0c616e36f0800f3ca47c599c641f376aa0
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-02-10 09:59:29 +0530

    Time: 1 ms (25.26%), Space: 9.1 MB (27.61%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    bool isDigit(char s) {
-        if (s == '0' || s == '1' || s == '2' || s == '3' || s == '4' ||
-            s == '5' || s == '6' || s == '7' || s == '8' || s == '9') {
-            return true;
-        }
-        return false;
-    }
     string clearDigits(string s) {
         stack<char> st;
         string res = "";
         st.push(s[0]);
 
         for (int i = 1; i < s.size(); i++) {
-            if (isDigit(s[i]) == true && !st.empty() && isDigit(st.top()) == false) {
+            if (s[i] >= '0' && s[i] <= '9' && !st.empty()) {
                 st.pop();
             } else {
                 st.push(s[i]);
