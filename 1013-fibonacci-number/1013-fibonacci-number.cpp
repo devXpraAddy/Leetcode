3654ad9281a7bcb8fd84d3f03f8361681972f99a
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-02-16 11:28:13 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.8%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
+    int t[31];
     int solve(int n){
         if(n== 0) return 0;
         if(n== 1) return 1;
 
+        if(t[n] != -1){
+            return t[n];
+        }
+
         int a = solve(n-1);
         int b = solve(n-2);
-        return a+b;
+        return t[n] = a+b;
     }
     int fib(int n) {
+        memset(t, -1, sizeof(t));
         return solve(n);
     }
 };
