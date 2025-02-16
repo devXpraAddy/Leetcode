4e55223e3e66315ac8cb5fa821aaa17d140dae8b
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-02-16 14:41:02 +0530

    Time: 69 ms (46.22%), Space: 8.9 MB (92.94%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[10001];
+    int solve(int n){
+        if(n== 0) return 0;
+
+        if(t[n] != -1){
+            return t[n];
+        }
+        int mincount = INT_MAX;
+
+        for(int i =1; i*i<=n; i++){
+            int res = 1 + solve(n - i*i);
+            mincount = min(mincount, res);
+        }
+        return t[n] = mincount;
+    }
+    int numSquares(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};
