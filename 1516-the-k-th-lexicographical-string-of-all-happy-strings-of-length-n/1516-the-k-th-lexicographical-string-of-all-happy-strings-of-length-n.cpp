8a42a3fe202d4e7782d8c7fec36b05eac5289e60
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-02-19 22:50:33 +0530

    Time: 13 ms (51.18%), Space: 24.5 MB (39.97%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,36 @@
+//T.C : O(n * 3 * 2^(n-1)) ~= O(n*2^n)
+//S.C : O(n * 2^n) , total 2^n strings eaching having length n
+class Solution {
+public:
+    void solve(int n, string &curr, vector<string> &result) {
+        if (curr.length() == n) {
+            result.push_back(curr);
+            return;
+        }
+        
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (!curr.empty() && curr.back() == ch)
+                continue;
+            
+            // Do
+            curr.push_back(ch);
+
+            // Explore
+            solve(n, curr, result);
+
+            // Undo
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        vector<string> result;
+        solve(n, curr, result);
+
+        if (result.size() < k) 
+            return "";
+        
+        return result[k - 1];
+    }
+};
