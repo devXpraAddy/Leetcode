f11f8af115c7043dd194aa089098143cf8f7d8d1
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-03-09 21:57:30 +0530

    Time: 68 ms (70.5%), Space: 108 MB (40.59%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -0,0 +1,34 @@
+//T.C : O(n+k)
+//S.C : O(1) (But you can mention you have to add k-1 extra elements so space complexity ideally should be O(k-1))
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+
+        int N = n + (k-1);
+        for(int i = 0; i < k-1; i++) {
+            colors.push_back(colors[i]); //to handle wrap around (circular array)
+        }
+
+        int result = 0;
+        int i = 0;
+        int j = 1; //because we have to check index j-1 for checking alterate
+        while(j < N) {
+            if(colors[j] == colors[j-1]) {
+                i = j;
+                j++;
+                continue;
+            }
+
+            if(j - i + 1 == k) {
+                result++;
+                i++;
+            }
+
+            j++;
+        }
+
+        return result;
+    }
+};
+
