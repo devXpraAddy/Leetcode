05f724640680395aa661aa9d4374a7faf287e83b
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-03-15 21:26:37 +0530

    Time: 19 ms (81.98%), Space: 60.8 MB (67.69%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -0,0 +1,39 @@
+//Approach-2 (Binary Search on Answer "Minimize the Maximum Robbed Amount(capability)")
+//T.C : O(n*log(maxC))
+//S.C : O(1)
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int mid, int k) {
+        int house = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] <= mid) {
+                house++;
+                i++; //skipping adjacent house
+            }
+        }
+
+        return house >= k; //we managed to rob atleast k houses
+    }
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int l = *min_element(begin(nums), end(nums));
+        int r = *max_element(begin(nums), end(nums));
+
+        int result = r;
+
+        while(l <= r) { //O(log(maxC) * n )
+            int mid = l + (r-l)/2; //capability
+
+            if(isPossible(nums, mid, k) == true) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+
+        return result;
+    }
+};
+
