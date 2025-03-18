d3d353e280b6879a291924895bd98f600c24703a
Snippet <115340624+devXpraAddy@users.noreply.github.com>
2025-03-18 07:32:10 +0530

    Time: 9 ms (9.56%), Space: 18.3 MB (9.45%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        vector<int> freq(501, 0);
-        for (int num : nums) {
-            freq[num]++;
+        unordered_map<int,int> mp;
+
+        for(int i= 0;i<nums.size(); i++){
+            mp[nums[i]]++;
         }
-        for (int count : freq) {
-            if (count % 2 != 0) return false;
+        for(int i = 0;i<nums.size(); i++){
+            if(mp[nums[i]] %2 != 0){
+                return false;
+            }
         }
         return true;
     }
