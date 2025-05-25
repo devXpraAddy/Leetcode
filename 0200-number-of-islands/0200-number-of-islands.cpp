f9fab3f43d4283bdc0a3b838c1884e43fa272a2a
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-05-25 18:08:27 +0530

    Time: 57 ms (12.2%), Space: 31 MB (8.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,51 +1,43 @@
 class Solution {
-private:
-    void bfs(int i, int j, vector<vector<char>>& grid,
-             vector<vector<int>>& vis) {
+public:
+    void bfs(int i, int j, vector<vector<int>>& vis, int n, int m, vector<vector<char>>& grid){
         vis[i][j] = 1;
-        queue<pair<int, int>> q;
-        q.push({i, j});
 
-        int n = grid.size();
-        int m = grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({i,j});
 
-
-
-        while (!q.empty()) { // iterate on queue
+        while(!q.empty()){
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
 
-            int delRow[] = {-1, 0, 1, 0};
-            int delCol[] = {0, 1, 0, -1};
+            vector<int>delRow = {-1, 0, 1, 0};
+            vector<int>delCol = {0, -1, 0, 1};
 
-            // traverse in the neighbour and mark them
-            for (int i = 0; i <4; i++) {
-                
-                    int nRow = row + delRow[i];
-                    int nCol = col + delCol[i];
-                    if (nRow >= 0 && nCol >= 0 && nRow < n && nCol < m &&
-                        grid[nRow][nCol] == '1' && !vis[nRow][nCol]) {
-                        vis[nRow][nCol] = 1;
-                        q.push({nRow, nCol});
+            for(int i = 0; i<4; i++){
+                int newRow = row + delRow[i];
+                int newCol = col + delCol[i];
+                if(newRow >=0 && newCol >=0 && newRow <n && newCol <m && grid[newRow][newCol] == '1' && !vis[newRow][newCol]){
+                    vis[newRow][newCol] =1;
+                    q.push({newRow, newCol});
                 }
-            }
+            }             
         }
     }
 
-public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
+
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
+        for(int i =0 ;i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
                     cnt++;
-                    bfs(i, j, grid, vis);
+                    bfs(i, j, vis, n, m, grid);
                 }
             }
         }
