b9ec52d4258bc3b6c381b05e7367f8e9a749aeaa
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-05-26 14:00:52 +0530

    Time: 0 ms (100%), Space: 18.1 MB (60.08%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,29 @@
 class Solution {
-public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, vector<vector<int>>& res, int newColor, int oldColor){
-        res[sr][sc] = newColor;
+private:
+    void dfs(vector<vector<int>>& image, int row, int col, int initColor, vector<vector<int>>& res, int finalColor){
+        res[row][col] = finalColor;
 
         int n = image.size();
         int m = image[0].size();
 
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1}; 
+        vector<int> delRow = {-1, 0, 1, 0};
+        vector<int> delCol = {0, -1, 0, 1};
 
         for(int i = 0;i<4; i++){
-                int nrow = sr + delRow[i];
-                int ncol = sc + delCol[i];
-                if(nrow >= 0 && ncol >= 0 && nrow <n && ncol <m && image[nrow][ncol] == oldColor && res[nrow][ncol] != newColor){
-                    dfs(image, nrow, ncol, res, newColor, oldColor);
+            int newRow = row + delRow[i];
+            int newCol = col + delCol[i];
+            if(newRow >=0 && newCol >=0 && newRow < n && newCol <m && image[newRow][newCol] == initColor && res[newRow][newCol] != finalColor){
+                dfs(image, newRow, newCol, initColor, res, finalColor);
             }
         }
+
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        vector<vector<int>> res = image; // making a copy to not modify initial matrix
-        int oldColor = image[sr][sc];
-        dfs(image, sr, sc, res, newColor, oldColor);
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int finalColor) {
+        int initColor = image[sr][sc];
+        vector<vector<int>> res = image;
+
+        dfs(image, sr, sc, initColor, res, finalColor);
         return res;
     }
 };
