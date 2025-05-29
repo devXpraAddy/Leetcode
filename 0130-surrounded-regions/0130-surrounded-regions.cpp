a17ef57d3b5cfb67c2785b0372b87f9adeb1086a
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-05-29 15:34:54 +0530

    Time: 2 ms (31.96%), Space: 15.3 MB (21.85%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,69 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>&vis){
+    void dfs(int row, int col, vector<vector<char>>& board,
+             vector<vector<int>>& vis) {
         vis[row][col] = 1;
 
         int n = board.size();
         int m = board[0].size();
 
-        vector<int>delRow = {-1, 0, 1, 0};
-        vector<int>delCol = {0, 1, 0, -1};
+        queue<pair<int, int>> q;
+        q.push({row, col});
 
-        for(int i = 0;i<4; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow >=0 && ncol >=0 && nrow <n && ncol <m && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
-                dfs(nrow, ncol, board, vis);
+        vector<int> delRow = {-1, 0, 1, 0};
+        vector<int> delCol = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int roww = q.front().first;
+            int coll = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = roww + delRow[i];
+                int ncol = coll + delCol[i];
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
+                    !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
             }
         }
     }
+
 public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
 
-        vector<vector<int>>vis(n, vector<int>(m, 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        for(int j = 0; j<m; j++){
+        for (int j = 0; j < m; j++) {
+            // check for unvisited Os in the boundary rows
+
             // First Row
-            if(!vis[0][j] && board[0][j] == 'O'){
+            if (!vis[0][j] && board[0][j] == 'O') {
                 dfs(0, j, board, vis);
             }
             // Last Row
-            if(!vis[n-1][j] && board[n-1][j] == 'O'){
-                dfs(n-1, j, board, vis);
+            if (!vis[n - 1][j] && board[n - 1][j] == 'O') {
+                dfs(n - 1, j, board, vis);
             }
         }
-        
-        for(int i =0; i<n; i++){
-            //First Column
-            if(!vis[i][0] && board[i][0] == 'O'){
+
+        for (int i = 0; i < n; i++) {
+            // check for unvisited Os in the boundary column
+
+            // First Column
+            if (!vis[i][0] && board[i][0] == 'O') {
                 dfs(i, 0, board, vis);
             }
-            if(!vis[i][m-1] && board[i][m-1] == 'O'){
-                dfs(i, m-1, board, vis);
+            if (!vis[i][m - 1] && board[i][m - 1] == 'O') {
+                dfs(i, m - 1, board, vis);
             }
         }
-        for(int i =0 ;i<n; i++){
-            for(int j = 0;j<m; j++){
-                if(!vis[i][j] && board[i][j] == 'O'){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
