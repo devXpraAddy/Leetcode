1b6bea74c8ef8ee6ce7d2dce5570764c04f5c1ab
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-06-04 16:27:54 +0530

    Time: 8 ms (78.21%), Space: 52.9 MB (74.58%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,13 +1,13 @@
 class Solution {
 private:
-    bool dfs(int node, vector<int>&vis, vector<int>&pathVis, vector<vector<int>>& graph, vector<int>&check){
+    bool dfs(int node, vector<int>&vis,vector<int>& pathVis, vector<int>& check, vector<vector<int>>& graph){
         vis[node] = 1;
         pathVis[node] = 1;
         check[node] = 0;
 
         for(auto it: graph[node]){
             if(!vis[it]){
-                if(dfs(it, vis, pathVis, graph, check) == true) return true;
+                if(dfs(it, vis, pathVis, check, graph) == true) return true;
             }else if(pathVis[it]){
                 return true;
             }
@@ -18,20 +18,21 @@ private:
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int N = graph.size();
-        vector<int>vis(N);
-        vector<int>pathVis(N);
-        vector<int>check(N);
+        int n = graph.size();
+
+        vector<int> vis(n);
+        vector<int> pathVis(n);
+        vector<int> check(n);
 
         vector<int>res;
 
-        for(int i=0; i<N; i++){
+        for(int i =0;i<n; i++){
             if(!vis[i]){
-                dfs(i, vis, pathVis, graph, check);
+                dfs(i, vis, pathVis, check, graph);
             }
         }
-        for(int i =0;i<N; i++){
-            if(check[i] ==1){
+        for(int i =0;i<n; i++){
+            if(check[i] == 1){
                 res.push_back(i);
             }
         }
