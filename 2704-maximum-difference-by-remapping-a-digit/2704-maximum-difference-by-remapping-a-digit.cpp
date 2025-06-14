851f9866792a5590e04e06681c76aaaa28067f75
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-06-14 21:19:14 +0530

    Time: 0 ms (100%), Space: 8.2 MB (37.74%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,36 @@
+//Approach-1 (Simple iterating on digits)
+//T.C : O(log10(n))
+//S.C : O(log10(n))
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string minNum = to_string(num);
+        string maxNum = to_string(num);
+        int n = minNum.length();
+
+        char ch = ' ';
+        for(int i = 0; i < n; i++) {
+            if(maxNum[i] != '9') {
+                ch = maxNum[i];
+                break;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(maxNum[i] == ch) {
+                maxNum[i] = '9';
+            }
+        }
+        
+
+        ch = minNum[0];
+        for(int i = 0; i < n; i++) {
+            if(minNum[i] == ch) {
+                minNum[i] = '0';
+            }
+        }
+
+        return stoi(maxNum) - stoi(minNum);
+
+    }
+};
