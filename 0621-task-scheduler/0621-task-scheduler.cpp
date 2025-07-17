47f730b4a958a3d9ac17a1bb12fb89d44744bd81
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-07-17 12:46:20 +0530

    Time: 40 ms (28.85%), Space: 47.8 MB (18.61%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,40 +3,38 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char, int> mp;
 
+        priority_queue<int> pq;
         for (int i = 0; i < tasks.size(); i++) {
             mp[tasks[i]]++;
         }
 
-        int time = 0;
-
-        priority_queue<int> pq;
-
         for (auto it : mp) {
-            if (it.second > 0) {
                 pq.push(it.second);
-            }
         }
+        int res = 0;
+
         while (!pq.empty()) {
             vector<int> temp;
             for (int i = 1; i <= n + 1; i++) {
                 if (!pq.empty()) {
+
                     int freq = pq.top();
                     pq.pop();
                     freq--;
                     temp.push_back(freq);
                 }
             }
-            for(int &f : temp){
-                if(f>0){
+            for (auto& f : temp) {
+                if (f > 0) {
                     pq.push(f);
                 }
             }
-            if(pq.empty()){
-                time += temp.size();
-            }else{
-                time += n+1;
+            if (pq.empty()) {
+                res += temp.size();
+            } else {
+                res += n + 1;
             }
         }
-        return time;
+        return res;
     }
 };
