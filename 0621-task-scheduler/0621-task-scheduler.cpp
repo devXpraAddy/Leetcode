7989385863f67be317994cfb0149c1fbdd6553c0
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-07-17 15:37:56 +0530

    Time: 41 ms (26.81%), Space: 47.7 MB (30.41%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,37 +2,39 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char, int> mp;
+        priority_queue<int>pq;
+        int res = 0;
 
-        priority_queue<int> pq;
-        for (int i = 0; i < tasks.size(); i++) {
+
+        for(int i =0; i<tasks.size(); i++){
             mp[tasks[i]]++;
         }
 
-        for (auto it : mp) {
-                pq.push(it.second);
+        for(auto it: mp){
+            pq.push(it.second);     
         }
-        int res = 0;
 
-        while (!pq.empty()) {
-            vector<int> temp;
-            for (int i = 1; i <= n + 1; i++) {
-                if (!pq.empty()) {
+        while(!pq.empty()){
+            vector<int>temp;
 
+            for(int i =1 ;i<=n+1; i++){
+                if(!pq.empty()){
                     int freq = pq.top();
                     pq.pop();
                     freq--;
                     temp.push_back(freq);
-                }
+                } 
             }
-            for (auto& f : temp) {
-                if (f > 0) {
+            for(auto f: temp){
+                if(f>0){
                     pq.push(f);
                 }
             }
-            if (pq.empty()) {
-                res += temp.size();
-            } else {
-                res += n + 1;
+
+            if(pq.empty()){
+                res+= temp.size();
+            }else{
+                res+= n+1;
             }
         }
         return res;
