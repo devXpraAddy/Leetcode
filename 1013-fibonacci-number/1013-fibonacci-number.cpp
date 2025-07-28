34afdd1dee692bec96e60042478010325581a5e4
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-07-28 22:45:18 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.6%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n== 0) return 0;
-        vector<int>dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
+        if(n== 0) return 0; // Don't forget this edge case
+        // vector<int>dp(n+1, -1);
+        int prev2 = 0;
+        int prev = 1;
 
         for(int i =2; i<=n; i++){
-            int left = dp[i-1];
-            int right = dp[i-2];
+            int left = prev;
+            int right = prev2;
 
-            dp[i] = left + right;
+            int curr = left + right;
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n];
+        return prev;
     }
 };
