39b9e8cbcd35cdf029316b2923347d17d487b461
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-07-28 23:28:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.95%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -3,19 +3,23 @@ public:
     int tribonacci(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
-        vector<int>dp(n+1, -1);
-        dp[0] =0;
-        dp[1] =1;
-        dp[2] =1;
+
+        // vector<int>dp(n+1, -1);
+        int prev3 =0;
+        int prev2 =1;
+        int prev =1;
 
         for(int i = 3; i<=n; i++){
-            int a = dp[i-1];
-            int b = dp[i-2];
-            int c = dp[i-3];
+            int a = prev;
+            int b = prev2;
+            int c = prev3;
 
-            dp[i] = a+b+c;
+            int curr = a+b+c;
+            prev3 = prev2;
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n];
+        return prev;
     }
 };
