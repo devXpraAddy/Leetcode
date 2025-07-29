0d6f40772dce4a68fa4852954def1e450dd2e498
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-07-29 12:07:18 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     int solve(int m, int n, vector<vector<int>>& dp){
-        if(m == 0 || n == 0) return 1;
+        if(m == 0 && n == 0) return 1;
         if(m<0 || n<0) return 0;
 
         if(dp[m][n] != -1){
