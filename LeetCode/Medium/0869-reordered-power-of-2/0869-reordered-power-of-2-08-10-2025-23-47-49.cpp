0cb1819409abe167eb5397362833a270c5625f6b
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-08-10 23:47:50 +0530

    Time: 3 ms (23.6%), Space: 9.1 MB (41.89%) - LeetHub

---
+++ LeetCode/Medium/0869-reordered-power-of-2/0869-reordered-power-of-2-08-10-2025-23-47-49.cpp
@@ -0,0 +1,25 @@
+//T.C : O(dlogd), d = number of digits
+//S.C : O(d)
+class Solution {
+public:
+    string getSortedStr(int n) {
+        string s = to_string(n);
+        sort(begin(s), end(s));
+        return s;
+    }
+
+    bool reorderedPowerOf2(int n) {
+        string s = getSortedStr(n);
+
+        //check all 2 powers and see if it matches with s
+        //2^29
+        for(int p = 0; p <= 29; p++) {
+            if(s == getSortedStr(1 << p)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
