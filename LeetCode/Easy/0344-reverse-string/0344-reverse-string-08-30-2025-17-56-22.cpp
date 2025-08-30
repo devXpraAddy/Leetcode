5d440fa8283d4715ff8fdd53d030d5e4e084cb17
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-08-30 17:56:22 +0530

    Time: 0 ms (100%), Space: 27.2 MB (51.28%) - LeetHub

---
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string-08-30-2025-17-56-22.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0, h = s.size()-1;
+        while(l<h){
+            int temp = s[l];
+            s[l] = s[h];
+            s[h] = temp;
+            l++;
+            h--;
+        }
+    }
+};
