f49200890e058b92de0cdd43dcbea3793e15df01
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-09-05 00:00:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.23%) - LeetHub

---
+++ LeetCode/Easy/3516-find-closest-person/3516-find-closest-person-09-05-2025-00-00-17.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int dist1 = abs(z-x);
+        int dist2 = abs(z-y);
+
+        if(dist1 < dist2){
+            return 1;
+        }else if(dist1 > dist2){
+            return 2;
+        }else{
+            return 0;
+        }
+    }
+};
