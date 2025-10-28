b222036f45181e34bed123e4c0635726c2afc20f
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-10-29 01:28:58 +0530

    Time: 133 ms (6.59%), Space: 43.6 MB (7.19%) - LeetHub

---
+++ LeetCode/Easy/3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero-10-29-2025-01-28-58.java
@@ -0,0 +1,40 @@
+//T.C : O(n^ * m) 
+//S.C : O(n)
+class Solution {
+    private boolean isValid(int[] nums, int count, int i, int direction) {
+        int[] temp = nums.clone();
+        int idx = i;
+
+        while (count > 0 && idx >= 0 && idx < temp.length) {
+            if (temp[idx] > 0) {
+                temp[idx]--;
+                direction *= -1;
+                if (temp[idx] == 0) {
+                    count--;
+                }
+            }
+            idx += direction;
+        }
+
+        return count == 0; // all elements become 0
+    }
+
+    public int countValidSelections(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num != 0) count++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (isValid(nums, count, i, -1)) result++;
+                if (isValid(nums, count, i, 1)) result++;
+            }
+        }
+
+        return result;
+    }
+}
