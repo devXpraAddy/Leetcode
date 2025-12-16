cc08e48cdcb2123608b2753e0a422992cde283eb
Aditya Prajapati <115340624+devXpraAddy@users.noreply.github.com>
2025-12-16 17:48:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx-12-16-2025-17-48-42.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x == 0) return 0;
+        
+        long long ans = 1;
+
+        for(int i =1; i<=x; i++){
+            if(i*i <= x){
+                ans = i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
